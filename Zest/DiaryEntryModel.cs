29b59abeac2a18b411379fb76cd889666c56ac3b
Jon Hamshaw <Jonathan_Hamshaw@mckinsey.com>
2016-01-27 20:13:15 +0000

    adding gitignore

---
+++ Zest/DiaryEntryModel.cs
@@ -0,0 +1,12 @@
+using System;
+using UIKit;
+using System.Collections.Generic;
+
+namespace Zest
+{
+	public class DiaryEntry
+	{
+	}
+
+}
+
---
+++ Zest/MasterTableViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Zest
+{
+	partial class MasterTableViewController : UITableViewController
+	{
+		public MasterTableViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
